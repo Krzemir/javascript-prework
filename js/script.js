ae5d97e2b227cb312c976a6a3a9ffa7621717268
Krzysztof Mirończuk <krzysztof.mironczuk@socart.media>
2022-04-07 08:47:18 +0200

    Change let to const

--- js/script.js
+++ js/script.js
@@ -16,39 +16,19 @@ function playGame(playerInput){
       }
     }
 
-
-  let randomNumber = Math.floor(Math.random() * 3 + 1);
+    const randomNumber = Math.floor(Math.random() * 3 + 1);
 
   console.log('Wylosowana liczba to: ' + randomNumber);
 
-  let computerMove = getMoveName(randomNumber);
-  /*
-  if(randomNumber == 1){
-    computerMove = 'kamień';
-  } else if(randomNumber == 2){
-      computerMove = 'papier';
-  } else if(randomNumber == 3){
-      computerMove = 'nożyce';
-  }*/
-
+  const computerMove = getMoveName(randomNumber);
+ 
   printMessage('Mój ruch to: ' + computerMove);
-
-  //let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
   console.log('Gracz wpisał: ' + playerInput);
 
-  let playerMove = getMoveName(playerInput);
+  const playerMove = getMoveName(playerInput);
 
   printMessage('Twój ruch to: ' + playerMove);
-
-  /*
-  if(playerInput == '1'){
-    playerMove = 'kamień';
-  } else if(playerInput == '2'){
-      playerMove = 'papier';
-  } else if(playerInput == '3'){
-      playerMove = 'nożyce';
-  }*/
 
   function displayResult(argComputerMove, argPlayerMove){
       if ((argComputerMove == 'kamień' && argPlayerMove == 'papier')
@@ -87,9 +67,6 @@ document.getElementById('play-paper').addEventListener('click', function(){
 document.getElementById('play-scissors').addEventListener('click', function(){
   playGame(3);
 });
-
-//Results of entire game:
-
 
 let playerScore = 0;
 let computerScore = 0;
